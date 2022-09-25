07c66f8e41279feca3229271c3726f2eccbff8de
luchkim <cheolhyok@gmail.com>
2022-09-25 03:57:17 -0500

    After done Login and out page

--- Code/Project/app_seed/register/views.py
+++ Code/Project/app_seed/register/views.py
@@ -4,13 +4,6 @@ from .forms import RegisterForm
 
 # Create your views here.
 def register(res):
-    if res.user.is_authenticated:
-        tot = '/logout'
-        dot = 'LOGOUT'
-    else:
-        tot = '/login'
-        dot = 'LOGIN'
-
     if res.method == 'POST':
         form = RegisterForm(res.POST)
         if form.is_valid():
